799bfc34e085d9713e0beffca47ec083f7493548
Ivy-Kang <mkang19@hawk.iit.edu>
2023-11-13 09:45:37 -0600

    add content to map.html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-
+function init(){
+  var el = document.getElementById('canvas');
+  var myLocation = new google.maps.LatLng(33.454072, 126.793135);
+  var mapOptions = {
+    center: myLocation,
+    zoom:18,
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    mapTypeControlOptions: {
+      position: google.maps.ControlPosition.BOTTOM_CENTER
+    }
+  };
+  
+  var myMap = new google.maps.Map(el, mapOptions);
+  
+  var marker = new google.maps.Marker({
+    position: myLocation,
+    map: myMap,
+    
